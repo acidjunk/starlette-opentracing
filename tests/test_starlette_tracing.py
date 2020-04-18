7670e6d0214e71382f4965dc1857eca6a005b7a1
Rene Dohmen <acidjunk@gmail.com>
2020-04-18 22:10:41 +0200

    First couple of tests

--- tests/test_starlette_tracing.py
+++ tests/test_starlette_tracing.py
@@ -1,3 +1,7 @@
+import pytest
+
 def test_view_ok(client):
+    """Test loading a starlette app with the trace enabled and query a view."""
     response = client.get("/foo/")
     assert response.status_code == 200
+    
